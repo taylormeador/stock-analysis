81164e9ced46863bf226fcbf28b4761506473aec
Taylor <taylor.r.meador@gmail.com>
2026-01-27 19:57:14 -0700

    use correct task name in beat

--- local/worker/app/main.py
+++ local/worker/app/main.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 # Celery Beat schedule
 app.conf.beat_schedule = {
     "scrape-reddit-hot": {
-        "task": "tasks.scraping.scrape_reddit_hot",
+        "task": "app.tasks.scraping.scrape_reddit_hot",
         "schedule": 300.0,
     },
 }
