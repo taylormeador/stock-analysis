6081659f8ea5566d97b023ecd97b289ad0aace18
Taylor <taylor.r.meador@gmail.com>
2026-01-31 16:22:22 -0700

    switch to using compressed file

--- local/scripts/extract_historical_reddit_comment_data.py
+++ local/scripts/extract_historical_reddit_comment_data.py
@@ -1,9 +1,12 @@
+import io
 import json
-import psycopg2
-from psycopg2.extras import execute_values
 import os
 import re
 from datetime import datetime, timezone
+
+import psycopg2
+import zstandard as zstd
+from psycopg2.extras import execute_values
 
 SUBREDDITS = {"wallstreetbets"}
 BATCH_SIZE = 1000
@@ -182,56 +185,62 @@ sql = """
 template = "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
 batch = []
-with open("/mnt/srv2-storage/reddit/reddit/comments/RC_2025-12", "r") as f:
-    for i, line in enumerate(f):
-        try:
-            l = json.loads(line)  # noqa: E741
-            if l.get("subreddit", "").lower() in SUBREDDITS:
-                permalink = l["permalink"]
-                fields = permalink.split("comments/")
-                post_id = fields[1].split("/")[0]
+with open("/mnt/srv2-storage/reddit/reddit/comments/RC_2025-12.zst", "rb") as fh:
+    dctx = zstd.ZstdDecompressor()
+    with dctx.stream_reader(fh) as reader:
+        text_stream = io.TextIOWrapper(reader, encoding="utf-8")
+        i = 0
+        for line in text_stream:
+            try:
+                l = json.loads(line)  # noqa: E741
+                if l.get("subreddit", "").lower() in SUBREDDITS:
+                    permalink = l["permalink"]
+                    fields = permalink.split("comments/")
+                    post_id = fields[1].split("/")[0]
 
-                created_utc = datetime.fromtimestamp(
-                    l["created_utc"],
-                    tz=timezone.utc,
-                )
+                    created_utc = datetime.fromtimestamp(
+                        l["created_utc"],
+                        tz=timezone.utc,
+                    )
 
-                comment = (
-                    l.get("id"),
-                    l.get("parent_id"),
-                    post_id,
-                    l["subreddit"],
-                    l["body"],
-                    l["score"],
-                    extract_ticker(l["body"]),
-                    l["controversiality"],
-                    l["author"],
-                    created_utc,
-                )
-                batch.append(comment)
+                    comment = (
+                        l.get("id"),
+                        l.get("parent_id"),
+                        post_id,
+                        l["subreddit"],
+                        l["body"],
+                        l["score"],
+                        extract_ticker(l["body"]),
+                        l["controversiality"],
+                        l["author"],
+                        created_utc,
+                    )
+                    batch.append(comment)
 
-                # Bulk insert to DB
-                if len(batch) >= BATCH_SIZE:
-                    with conn.cursor() as cur:
-                        execute_values(cur, sql, batch, template)
-                        conn.commit()
-                    batch = []
-                    print("inserted batch")
+                    # Bulk insert to DB
+                    if len(batch) >= BATCH_SIZE:
+                        with conn.cursor() as cur:
+                            execute_values(cur, sql, batch, template)
+                            conn.commit()
+                        batch = []
+                        print("inserted batch")
 
-            if i % 100000 == 0:
-                print(f"Processed {i:,} lines")
+                if i % 100000 == 0:
+                    print(f"Processed {i:,} lines")
 
-        except json.JSONDecodeError:
-            continue
+            except json.JSONDecodeError:
+                continue
 
-        except Exception as e:
-            print(e)
-            breakpoint()
+            except Exception as e:
+                print(e)
+                breakpoint()
 
-    if batch:
-        with conn.cursor() as cur:
-            execute_values(cur, sql, batch, template)
-            conn.commit()
-            print("inserted final partial batch")
+            i += 1
 
-    print("ETL complete")
+        if batch:
+            with conn.cursor() as cur:
+                execute_values(cur, sql, batch, template)
+                conn.commit()
+                print("inserted final partial batch")
+
+        print("ETL complete")
