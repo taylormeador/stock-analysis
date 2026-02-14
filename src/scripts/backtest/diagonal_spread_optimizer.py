b137092de5b65e2c09a3a9047641580bcd274637
Taylor <taylor.r.meador@gmail.com>
2026-02-14 08:05:34 -0700

    trying to use bad data = bad time

--- src/scripts/backtest/diagonal_spread_optimizer.py
+++ src/scripts/backtest/diagonal_spread_optimizer.py
@@ -1,9 +1,18 @@
 import itertools
+import logging
 from datetime import timedelta
 
 import db as db
 import pandas as pd
 from sqlalchemy import text
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
 
 TICKERS = [
     "SPY",
@@ -70,14 +79,90 @@ def get_options_data(ticker: str, start_date: str = "2021-01-01"):
 
 PARAMS = {
     "long_delta": [0.9, 0.8, 0.7, 0.6],
-    "long_dte": [180, 270, 365],
+    "long_dte": [60],
     "short_delta": [0.2, 0.3],
-    "short_dte": [21, 30, 45],
+    "short_dte": [14, 21],
     "short_close": [0.5, 0.75],  # percent of max profit of short call
     "short_roll": [0.4, 0.5],  # delta at which to roll short strike
     "stop_loss": [-0.1, -0.2, -0.3],  # percent decrease of entry debit
     "close_at": [0.25, 0.50, 0.75],  # percent profit of entry debit
 }
+
+
+class DiagonalSpread:
+    def __init__(self, long_call: pd.Series, short_call: pd.Series):
+        self.long_call = long_call
+        self.short_call = short_call
+
+        self._log_entry()
+        self.initial_debit = self._calc_price()
+        self.current_value = self.initial_debit
+
+    def _calc_price(
+        self,
+        long_slippage: float = 0.75,
+        short_slippage: float = 0.25,
+    ) -> float:
+        """
+        Calculate the value of the spread, assuming fills happen
+        closer to bid and ask for short and long calls, respectively.
+        """
+        long_spread = self.long_call.ask - self.long_call.bid
+        if (
+            long_spread > 1
+        ):  # TODO what threshold do we want to maybe not trade an option at?
+            logger.warning(f"Illiquid option bid/ask spread: ${long_spread}")
+
+        approx_long_slippage = long_spread * long_slippage
+        long_price = round(self.long_call.bid + approx_long_slippage, 2)
+        logger.debug(f"long_price: ${long_price}")
+
+        short_spread = self.short_call.ask - self.short_call.bid
+        if short_spread > 1:
+            logger.warning(f"Illiquid option bid/ask spread: ${short_spread}")
+
+        approx_short_spread = short_spread * short_slippage
+        short_price = round(self.short_call.bid + approx_short_spread, 2)
+        logger.debug(f"short_price: ${short_price}")
+
+        value = long_price - short_price
+        logger.info(f"Spread value: ${value}")
+
+        logger.info("*" * 100)
+
+        return value
+
+    def _log_entry(self):
+        logger.info("*" * 100)
+        logger.info("Opening position")
+        logger.info(f"long_call:\n{self.long_call}")
+        logger.info(f"short_call:\n{self.short_call}")
+
+    def calc_position_value(self, chain: pd.Series) -> float:
+        """
+        Calculate the value of the position on the date given by `chain`.
+        `chain` is a row from the option chain data for one day.
+        The procedure is done by finding our options in the current chain,
+        and updating instance vars with the new data, then running the calculation.
+        """
+        logger.info(f"Calculating position value on {chain.date.iloc[0]}")
+        expiration_mask = chain.expiration == self.long_call.expiration
+        strike_mask = chain.strike == self.long_call.strike
+        long_call = chain[expiration_mask & strike_mask]
+        if len(long_call) != 1:
+            raise ValueError("Something wrong while finding long call value")
+
+        self.long_call = long_call.iloc[0]
+
+        expiration_mask = chain.expiration == self.short_call.expiration
+        strike_mask = chain.strike == self.short_call.strike
+        short_call = chain[expiration_mask & strike_mask]
+        if len(chain[expiration_mask & strike_mask]) != 1:
+            raise ValueError("Something wrong while finding short call value")
+
+        self.short_call = short_call.iloc[0]
+
+        return self._calc_price()
 
 
 def run_strategy(params, df):
@@ -103,26 +188,38 @@ def run_strategy(params, df):
         if not position:
             today = df[df.date == current_date]
 
-            breakpoint()
-
             # Get the calls with the closest dte/delta to target
             dte_deltas = (
                 today.expiration - current_date - timedelta(days=long_dte)
             ).abs()
-            right_dte = df[dte_deltas == dte_deltas.min()]
+            right_dte = today[dte_deltas == dte_deltas.min()]
             delta_deltas = (long_delta - right_dte.delta).abs()
+            # TODO what if there are no calls? e.g. we are at the end of the dataset
             long_call = df.iloc[delta_deltas.idxmin()]
 
             dte_deltas = (
-                today.expiration - current_date - timedelta(days=long_dte)
+                today.expiration - current_date - timedelta(days=short_dte)
             ).abs()
-            right_dte = df[dte_deltas == dte_deltas.min()]
+            right_dte = today[dte_deltas == dte_deltas.min()]
             delta_deltas = (short_delta - right_dte.delta).abs()
             short_call = df.iloc[delta_deltas.idxmin()]
 
-            position = "test"
+            position = DiagonalSpread(long_call, short_call)
 
-        breakpoint()
+        # Calculate value of position on this day
+        # If below stop_loss, close position and end run
+        # If short_roll or short_close is triggered, do that
+        # If close_at is triggered, do that
+        current_chain = df[df.date == current_date]
+
+        # TODO this is a temporary check to get around bad data quality
+        if position.long_call.strike not in current_chain.strike.unique():
+            logger.info(f"skipping day {current_date}")
+            continue
+        if position.short_call.strike not in current_chain.strike.unique():
+            logger.info(f"skipping day {current_date}")
+            continue
+        position.current_value = position.calc_position_value(current_chain)
 
 
 def main():
