85eaae4c808a7184e7a3b2811cea79bff5b98657
Taylor <taylor.r.meador@gmail.com>
2026-02-14 11:04:03 -0700

    use redis url directly

--- src/worker/app/celery_app.py
+++ src/worker/app/celery_app.py
@@ -4,5 +4,5 @@ from celery import Celery
 
 # Celery configuration
 app = Celery("stock_analysis")
-app.conf.broker_url = os.getenv("CELERY_BROKER_URL")  # type: ignore
-app.conf.result_backend = os.getenv("CELERY_RESULT_BACKEND")  # type: ignore
+app.conf.broker_url = os.getenv("REDIS_URL")  # type: ignore
+app.conf.result_backend = os.getenv("REDIS_URL")  # type: ignore
