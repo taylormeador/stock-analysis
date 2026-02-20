dad7c9cef62c886ac4c765c80900f8d7f12ae443
Taylor Meador <taylormeador@Taylors-Mac-mini.local>
2026-02-19 17:55:56 -0700

    fix script task name

--- src/scripts/trigger_eod_options.sh
+++ src/scripts/trigger_eod_options.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -X POST http://10.0.10.127:5555/api/task/async-apply/app.tasks.apis.get_eod_option_data \
+curl -X POST http://10.0.10.127:5555/api/task/async-apply/app.tasks.apis.get_eod_options_data \
   -H "Content-Type: application/json" \
-  -d '{"kwargs": {"start_date": "2019-01-01", "end_date": "2019-12-31"}}'
+  -d '{"kwargs": {"start_date": "2016-01-01", "end_date": "2016-12-31"}}'
