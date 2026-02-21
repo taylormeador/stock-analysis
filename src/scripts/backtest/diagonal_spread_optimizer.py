0f0ce521321a8d0c498bb044fe6826fcedf4a2ea
Taylor Meador <taylormeador@Taylors-Mac-mini.local>
2026-02-21 08:54:31 -0700

    run backtest for one parameter set in diagonal spread optimizer

--- src/scripts/backtest/diagonal_spread_optimizer.py
+++ src/scripts/backtest/diagonal_spread_optimizer.py
@@ -41,26 +41,25 @@ TICKERS = [
 ]
 
 
-def get_options_data(ticker: str, start_date: str = "2021-01-01"):
+def get_options_data(
+    ticker: str,
+    start_date: str = "2021-01-01",
+    end_date: str = "2021-12-31",
+):
     sql = f"""
         SELECT
-            date,
+            quote_date,
             ticker,
+            underlying_price,
             expiration,
             strike,
-            call_put,
             bid,
             ask,
-            vol,
-            delta,
-            gamma,
-            theta,
-            vega,
-            rho
-        FROM pnp_option_chain
+            delta
+        FROM td_eod_options
         WHERE
-            date > '{start_date}' AND
-            call_put = 'Call' AND
+            quote_date BETWEEN '{start_date}' AND '{end_date}' AND
+            call_put = 'CALL' AND
             ticker = '{ticker}';
     """
     with db.get_connection() as conn:
@@ -68,54 +67,52 @@ def get_options_data(ticker: str, start_date: str = "2021-01-01"):
         data = result.fetchall()
 
     df = pd.DataFrame(data)
-
-    float_cols = ["bid", "ask", "vol", "delta", "gamma", "theta", "vega", "rho"]
+    float_cols = ["underlying_price", "bid", "ask", "delta"]
     df[float_cols] = df[float_cols].astype(float)
 
     return df
 
 
-# TODO output metrics, log to mlflow
-
-PARAMS = {
-    "long_delta": [0.9, 0.8, 0.7, 0.6],
-    "long_dte": [60],
-    "short_delta": [0.2, 0.3],
-    "short_dte": [14, 21],
-    "short_close": [0.5, 0.75],  # percent of max profit of short call
-    "short_roll": [0.4, 0.5],  # delta at which to roll short strike
-    "stop_loss": [-0.1, -0.2, -0.3],  # percent decrease of entry debit
-    "close_at": [0.25, 0.50, 0.75],  # percent profit of entry debit
-}
-
-
 class DiagonalSpread:
-    def __init__(self, long_call: pd.Series, short_call: pd.Series):
+    def __init__(
+        self,
+        long_call: pd.Series,
+        short_call: pd.Series,
+        stop_loss: float,
+        profit_target: float,
+    ):
         self.long_call = long_call
         self.short_call = short_call
 
-        self._log_entry()
-        self.initial_debit = self._calc_price()
+        self.initial_debit, self.initial_long_value, self.initial_short_value = (
+            self._calc_price()
+        )
         self.current_value = self.initial_debit
+        self.current_long_value = self.initial_long_value
+        self.current_short_value = self.initial_short_value
+        self.stop_loss_value = self.initial_debit * (1 - stop_loss)
+        self.profit_target_value = self.initial_debit * (1 + profit_target)
+        self.pnl = 0.0
+
+        self._log_entry()
 
     def _calc_price(
         self,
         long_slippage: float = 0.75,
         short_slippage: float = 0.25,
-    ) -> float:
+    ) -> tuple[float, float, float]:
         """
         Calculate the value of the spread, assuming fills happen
         closer to bid and ask for short and long calls, respectively.
         """
         long_spread = self.long_call.ask - self.long_call.bid
-        if (
-            long_spread > 1
-        ):  # TODO what threshold do we want to maybe not trade an option at?
+        # TODO what threshold do we want to maybe not trade an option at?
+        if long_spread > 1:
             logger.warning(f"Illiquid option bid/ask spread: ${long_spread}")
 
         approx_long_slippage = long_spread * long_slippage
         long_price = round(self.long_call.bid + approx_long_slippage, 2)
-        logger.debug(f"long_price: ${long_price}")
+        logger.debug(f"Long value: ${long_price}")
 
         short_spread = self.short_call.ask - self.short_call.bid
         if short_spread > 1:
@@ -123,29 +120,32 @@ class DiagonalSpread:
 
         approx_short_spread = short_spread * short_slippage
         short_price = round(self.short_call.bid + approx_short_spread, 2)
-        logger.debug(f"short_price: ${short_price}")
+        logger.debug(f"Short value: ${short_price}")
 
         value = long_price - short_price
         logger.info(f"Spread value: ${value}")
 
         logger.info("*" * 100)
 
-        return value
+        return value, long_price, short_price
 
     def _log_entry(self):
         logger.info("*" * 100)
         logger.info("Opening position")
         logger.info(f"long_call:\n{self.long_call}")
         logger.info(f"short_call:\n{self.short_call}")
+        logger.info(f"spread value: {self.current_value}")
+        logger.info(f"stop loss value: {self.stop_loss_value}")
+        logger.info(f"profit target value: {self.profit_target_value}")
 
-    def calc_position_value(self, chain: pd.Series) -> float:
+    def calc_position_value(self, chain: pd.Series):
         """
         Calculate the value of the position on the date given by `chain`.
         `chain` is a row from the option chain data for one day.
         The procedure is done by finding our options in the current chain,
         and updating instance vars with the new data, then running the calculation.
         """
-        logger.info(f"Calculating position value on {chain.date.iloc[0]}")
+        logger.info(f"Calculating position value on {chain.quote_date.iloc[0]}")
         expiration_mask = chain.expiration == self.long_call.expiration
         strike_mask = chain.strike == self.long_call.strike
         long_call = chain[expiration_mask & strike_mask]
@@ -162,7 +162,7 @@ class DiagonalSpread:
 
         self.short_call = short_call.iloc[0]
 
-        return self._calc_price()
+        self.current_value, self.long_value, self.short_value = self._calc_price()
 
 
 def run_strategy(params, df):
@@ -173,20 +173,23 @@ def run_strategy(params, df):
     short_close = params[4]
     short_roll = params[5]
     stop_loss = params[6]
-    close_at = params[7]
+    profit_target = params[7]
+
+    logger.info(f"running strategy with params: {params}")
 
     # TODO I think we need to make start date an input parameter for each position
-    start_date = df.date.min()
+    # TODO does it make more sense to loop over quote_date.unique()
+    start_date = df.quote_date.min()
     dates = [start_date + timedelta(days=i) for i in range(long_dte + 1)]
     position = None
     for i, current_date in enumerate(dates):
         # Skip days with no options data
-        if current_date not in df.date.unique():
+        if current_date not in df.quote_date.unique():
             continue
 
         # On the first day of the simulation, open the spread
         if not position:
-            today = df[df.date == current_date]
+            today = df[df.quote_date == current_date]
 
             # Get the calls with the closest dte/delta to target
             dte_deltas = (
@@ -195,6 +198,7 @@ def run_strategy(params, df):
             right_dte = today[dte_deltas == dte_deltas.min()]
             delta_deltas = (long_delta - right_dte.delta).abs()
             # TODO what if there are no calls? e.g. we are at the end of the dataset
+            # TODO optimize which call we buy - more liquid/closer to even multiple of 5 means better fill and is more realistic.
             long_call = df.iloc[delta_deltas.idxmin()]
 
             dte_deltas = (
@@ -204,22 +208,84 @@ def run_strategy(params, df):
             delta_deltas = (short_delta - right_dte.delta).abs()
             short_call = df.iloc[delta_deltas.idxmin()]
 
-            position = DiagonalSpread(long_call, short_call)
+            position = DiagonalSpread(long_call, short_call, stop_loss, profit_target)
 
-        # Calculate value of position on this day
+        # Calculate value of position on this da
+        today = df[df.quote_date == current_date]
+        position.calc_position_value(today)
+
         # If below stop_loss, close position and end run
-        # If short_roll or short_close is triggered, do that
-        # If close_at is triggered, do that
-        current_chain = df[df.date == current_date]
+        if position.current_value < position.stop_loss_value:
+            logger.info("*" * 100)
+            logger.info(f"Stop loss reached on {current_date}")
+            logger.info(f"Position value: {position.current_value}")
+            logger.info(f"Stop loss value: {position.stop_loss_value}")
+            logger.info("*" * 100)
+            return position
+            # TODO end run
 
-        # TODO this is a temporary check to get around bad data quality
-        if position.long_call.strike not in current_chain.strike.unique():
-            logger.info(f"skipping day {current_date}")
-            continue
-        if position.short_call.strike not in current_chain.strike.unique():
-            logger.info(f"skipping day {current_date}")
-            continue
-        position.current_value = position.calc_position_value(current_chain)
+        # If profit target is reached, close position and end run
+        if position.current_value > position.profit_target_value:
+            logger.info("*" * 100)
+            logger.info(f"Profit target reached on {current_date}")
+            logger.info(f"Position value: {position.current_value}")
+            logger.info(f"Profit target value: {position.profit_target_value}")
+            logger.info("*" * 100)
+            return position
+
+        # If the short call hits the profit target, roll down and out
+        if position.short_value < position.initial_short_value * (1 - short_close):
+            logger.info("*" * 100)
+            logger.info("Rolling call down and out")
+
+            # Bank the "profit"
+            profit = position.initial_short_value - position.short_value
+            position.pnl += profit
+
+            # Find a new short call
+            dte_deltas = (
+                today.expiration - current_date - timedelta(days=short_dte)
+            ).abs()
+            right_dte = today[dte_deltas == dte_deltas.min()]
+            delta_deltas = (short_delta - right_dte.delta).abs()
+            short_call = df.iloc[delta_deltas.idxmin()]
+
+            # Update the position to include the short call
+            # Recalculate position value to populate correct instance vars
+            position.short_call = short_call
+            position.calc_position_value(today)
+            position.initial_short_value = position.current_short_value
+
+        # If the short call is being threatened, roll up and out
+        # TODO this is tricky because in reality I would try to roll for a tiny credit,
+        # not necessarily based on strike/expiration
+        elif position.short_call.delta > short_roll:
+            logger.info("*" * 100)
+            logger.info("Rolling call up and out")
+
+            # Take the "loss"
+            # TODO is this necessarily a loss? Can it be that delta has increased from our entry but the value of the option has decreased?
+            # This could happen due to vega/IV in some cases I think
+            loss = position.initial_short_value - position.short_value
+            position.pnl += loss
+
+            # Find a new short call
+            dte_deltas = (
+                today.expiration - current_date - timedelta(days=short_dte)
+            ).abs()
+            right_dte = today[dte_deltas == dte_deltas.min()]
+            delta_deltas = (short_delta - right_dte.delta).abs()
+            short_call = df.iloc[delta_deltas.idxmin()]
+
+            # Update the position to include the short call
+            # Recalculate position value to populate correct instance vars
+            position.short_call = short_call
+            position.calc_position_value(today)
+            position.initial_short_value = position.current_short_value
+
+        # TODO implement closing short call for DTE proximity
+
+    return position
 
 
 def main():
@@ -232,12 +298,27 @@ def main():
     The short call will be rolled (defensively) at short_roll delta. If it cannot be rolled to the desired strike/DTE TODO
     The entire position will be closed and the run ended at close_at % profit of the entry debit.
     """
+
+    # TODO output metrics, log to mlflow
+
+    PARAMS = {
+        "long_delta": [0.9, 0.8, 0.7, 0.6],
+        "long_dte": [180, 270, 365],
+        "short_delta": [0.2, 0.3],
+        "short_dte": [28, 35, 42],
+        "short_close_delta": [0.5, 0.75],  # percent of max profit of short call
+        "short_close_dte": [7, 14, 21],  # DTE at which to close short call
+        "short_roll": [0.4, 0.5],  # delta at which to roll short strike
+        "stop_loss": [0.05, 0.1, 0.2, 0.3],  # percent decrease of entry debit
+        "profit_target": [0.25, 0.50, 0.75],  # percent profit of entry debit
+    }
     # for ticker in TICKERS:
     param_combos = itertools.product(*PARAMS.values())
     for ticker in ("SPY", "AAPL"):
         options_df = get_options_data(ticker)
         for params in param_combos:
-            run_strategy(params, options_df)
+            end_position = run_strategy(params, options_df)
+            breakpoint()
 
 
 if __name__ == "__main__":
