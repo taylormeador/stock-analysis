7d8fa79cb0bd6dbc8e38604e76ce3fc637f73be0
Taylor Meador <taylormeador@Taylors-Mac-mini.local>
2026-02-23 08:06:53 -0700

    write task status to db

--- backtester/src/main.rs
+++ backtester/src/main.rs
@@ -1,8 +1,9 @@
 use postgres::{Client, NoTls};
-use chrono::NaiveDate;
+use chrono::{NaiveDate, DateTime, Utc};
 use dotenvy::dotenv;
 use std::env;
 use rust_decimal::Decimal;
+use std::fmt;
 
 #[derive(Debug)]
 struct OptionContract {
@@ -15,6 +16,23 @@ struct OptionContract {
     delta: Decimal,
 }
 
+enum TaskStatus {
+    InProgress,
+    Failed,
+    Complete,
+}
+
+impl fmt::Display for TaskStatus {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TaskStatus::InProgress => write!(f, "In Progress"),
+            TaskStatus::Failed => write!(f, "Failed"),
+            TaskStatus::Complete => write!(f, "Complete"),
+        }
+    }
+}
+
+
 fn main() {
     dotenv().ok();
 
@@ -25,6 +43,7 @@ fn main() {
 
     println!("Connected to database");
 
+    // Read data
     let rows = client.query(
         "SELECT ticker, quote_date, expiration, strike, bid, ask, delta 
          FROM td_eod_options
@@ -45,4 +64,27 @@ fn main() {
         };
         println!("{:?}", contract);
     }
+
+    // Write data
+    let task_id = "test-task-id";
+    let component_name = "Backtest Worker";
+    let task_description = "Blazingly fast backtests";
+    let status = TaskStatus::InProgress;
+    let status_message = "Rust is tight";
+    let progress: f64 = 0.0420;
+    let start_time: DateTime<Utc> = Utc::now();
+
+    let sql = "
+        INSERT INTO etl_task_status (
+            task_id,
+            component_name,
+            task_description,
+            status,
+            status_message,
+            progress,
+            start_time
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7);
+         ";
+    client.execute(sql, &[&task_id, &component_name, &task_description, &status.to_string(), &status_message, &progress, &start_time]).expect("Error writing data to db");
+
 }
