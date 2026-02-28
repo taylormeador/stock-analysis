fb9bd9c5d6e260bd8cf696471dadc99211a0da28
Taylor Meador <taylormeador@Taylors-Mac-mini.local>
2026-02-28 07:10:23 -0700

    pass task_id

--- worker/app/tasks/backtests.py
+++ worker/app/tasks/backtests.py
@@ -6,10 +6,16 @@ from app.main import app
 logger = logging.getLogger(__name__)
 
 
-@app.task(queue="long-running")
-def run_backtest(ticker: str, start_date: str, end_date: str):
+@app.task(bind=True, queue="long-running")
+def run_backtest(self, ticker: str, start_date: str, end_date: str):
     process = subprocess.Popen(
-        ["/usr/local/bin/backtester", ticker, start_date, end_date],
+        [
+            "/usr/local/bin/backtester",
+            self.request.task_id,
+            ticker,
+            start_date,
+            end_date,
+        ],
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         text=True,
